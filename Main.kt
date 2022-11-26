5ccbf81acd93cc1a81f80828a3cf749b9ef80746
André Luis de Andrade <al.andrade256@gmail.com>
2022-11-26 13:28:01 -0300

    Stage 3/6

--- Main.kt
+++ Main.kt
@@ -5,15 +5,11 @@ class SearchEngine(private val text: MutableList<String>) {
         return listLine.indexOf(word)
     }
 
-
-
     fun findPosition(word: String, line: String): String {
         val index = getIndex(word, line)
 
         return if (index == -1) "Not found" else index.toString()
     }
-
-
 
     fun search(word: String): MutableList<String> {
         val found = mutableListOf<String>()
@@ -28,11 +24,10 @@ class SearchEngine(private val text: MutableList<String>) {
     }
 }
 
-class CLI() {
+class PeopleUseCase() {
     private val people = mutableListOf<String>()
     val se = SearchEngine(people)
-
-    fun readPeople() {
+    fun read() {
         println("Enter the number of people:")
         val peopleCount = readln().toInt()
 
@@ -42,31 +37,63 @@ class CLI() {
         }
     }
 
-    fun searchPeople() {
-        println("Enter the number of search queries:")
-        val queriesCount = readln().toInt()
+    fun find() {
+        println("Enter a name or email to search all suitable people.")
 
-        for (i in 1..queriesCount) {
-            println("Enter data to search people")
+        val query = readln()
+        val resultQuery = se.search(query)
 
-            val query = readln()
-            val resultQuery = se.search(query)
-
-            if (resultQuery.isNotEmpty()) {
-                println("People found:")
-                for (person in resultQuery) {
-                    println(person)
-                }
-            } else {
-                println("No matching people found.")
-            }
+        if (resultQuery.isNotEmpty()) {
+            println("People found:")
+            resultQuery.map { println(it) }
+        } else {
+            println("No matching people found.")
         }
+    }
+
+    fun print() {
+        println("\n=== List of people ===")
+
+        people.map { println(it) }
+    }
+}
+
+class CLI() {
+
+    private val peopleUseCase = PeopleUseCase()
+
+    fun menu() {
+        peopleUseCase.read()
+
+        do {
+            println("\n=== Menu ===")
+            println("1. Find a person")
+            println("2. Print all people")
+            println("0. Exit")
+
+            val command = readln().toInt()
+
+            when (command) {
+                1 -> {
+                    peopleUseCase.find()
+                }
+                2 -> {
+                    peopleUseCase.print()
+                }
+                0 -> {
+                    println("\nBye!\n")
+                    break
+                }
+                else -> {
+                    println("Incorrect option! Try again.")
+                }
+            }
+        } while (command != 0)
     }
 }
 
 fun main(args: Array<String>) {
     val cli = CLI()
 
-    cli.readPeople()
-    cli.searchPeople()
+    cli.menu()
 }
